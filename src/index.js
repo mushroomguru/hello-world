9d751d106977fe428ef060ba5bf48d4c31d6ad33
unknown <mushroomtreatment@gmail.com>
2022-11-10 12:03:42 +0000

    first commit

--- src/index.js
+++ src/index.js
@@ -5,11 +5,56 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+
+const mainDiv = <div>
+
+<p>This is the first paragraph</p>
+<p>This is the second paragraph</p>
+<p>This is the first paragraph</p>
+<p>This is the first paragraph</p>
+<p>This is the first paragraph</p>
+
+
+
+
+
+
+
+</div>
+
+
+root.render(mainDiv);
+
+// Version 2
+// const p1 = <p id="firstPara">This is a paraghraph</p>
+// const p2 = <p>This is a paragraph 2</p>
+// const myList = <ul>
+// <li>One</li>
+// <li>Two</li>
+// <li>Three</li>
+// <li>Four</li>
+// <li>Five</li>
+// </ul>
+// const mainDiv = <div>
+//   {p1}{p2}{myList}
+// </div>
+
+// root.render(mainDiv);
+
+
+//Version 1
+//const p1 = React.createElement("p",null,"This is paragraph 1");
+// const p1 = React.createElement("p",{id: "firstPara"},"This is paragraph 1");
+// const mainDiv = React.createElement("div",null,p1);
+// root.render(mainDiv);
+
+//<p>This is paragraph 1</p>
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
